9f937ee918efafb69f64fad77fa44652f4b8a7c1
tdirtyonthebeat <tdirtyonthebeat@gmail.com>
2026-08-29 15:34:41 -0500

    Let SIREN consume Creature State Bus events

--- Source/HorrorCastle/SirenEngine.h
+++ Source/HorrorCastle/SirenEngine.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <JuceHeader.h>
+#include "CreatureStateBus.h"
 #include <array>
 
 namespace horrorcastle {
@@ -9,7 +10,13 @@ class SirenEngine
 public:
     static constexpr int Jets = 4;
     struct JetState { float phase=0.0f; float displacement=0.0f; float edgeMemory=0.0f; };
-    struct VoiceState { std::array<JetState,Jets> jets{}; float plenum=0.0f; float field=0.0f; };
+    struct VoiceState {
+        std::array<JetState,Jets> jets{};
+        float plenum=0.0f;
+        float field=0.0f;
+        float ecologicalImpulse=0.0f;
+        CreatureStateBus::State creatureState{};
+    };
 
     float renderSample(VoiceState& state,
                        float fundamentalHz,
@@ -17,7 +24,11 @@ public:
                        float aether,
                        float expression,
                        float velocity,
-                       double sampleRate) noexcept;
+                       double sampleRate,
+                       float externalEvent=0.0f,
+                       float eventCoupling=0.0f) noexcept;
+
+    static const CreatureStateBus::State& stateBus(const VoiceState& state) noexcept { return state.creatureState; }
 };
 
 } // namespace horrorcastle
